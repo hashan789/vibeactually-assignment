ddacafdb838f8b6b13ef43a88f2093365e981017
hashan789 <hashanmalind123@gmail.com>
2025-09-27 14:36:31 +0530

    imported learning assistant

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import LearningAssistant from "./pages/Homepage";
+
+export default function Home() {
+  return (
+    <div className="font-sans">
+      <LearningAssistant />
+    </div>
+  );
+}
